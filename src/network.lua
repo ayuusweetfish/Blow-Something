f0234654571afa0f0624176bf938970d58a60ee5
Ayu <claire@ayu.land>
2025-01-27 01:34:13 +0800

    Fix incorrect URLs and language model prompts

--- src/network.lua
+++ src/network.lua
@@ -9,7 +9,7 @@ while true do
 
   local resp = {}
   http.request {
-    url = 'http://localhost:25126/look',
+    url = 'http://blow.ayu.land/look',
     method = 'POST',
     headers = { ['Content-Length'] = #msg },
     source = ltn12.source.string(msg),
