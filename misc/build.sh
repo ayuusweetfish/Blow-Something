f679aed75380b1d039e7af49b4ee5408f38ba819
Ayu <claire@ayu.land>
2025-01-27 01:33:44 +0800

    Revert to compatibility mode

--- misc/build.sh
+++ misc/build.sh
@@ -8,7 +8,7 @@ ${BOON} build . --target ${TARGET}
 
 # Web
 rm -rf release/Blow-Something-web
-${NODE} ${LOVEJS_INDEX} -t "Blow Something" -m 64000000 "release/Blow Something.love" release/Blow-Something-web
+${NODE} ${LOVEJS_INDEX} -c -t "Blow Something" -m 64000000 "release/Blow Something.love" release/Blow-Something-web
 cp misc/web_index.html release/Blow-Something-web/index.html
 # Patch for filesystem access
 sed -i '' 's/var SYSCALLS/try{if(!window.FS)window.FS=FS;}catch(e){}var SYSCALLS/' release/Blow-Something-web/love.js
