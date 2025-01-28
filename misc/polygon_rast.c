ad1b7934703e27917633315caac71bed6b842207
Ayu <claire@ayu.land>
2025-01-28 17:42:55 +0800

    Draw outlines (preliminary)

--- misc/polygon_rast.c
+++ misc/polygon_rast.c
@@ -2,15 +2,32 @@
 #include <emscripten/emscripten.h>
 #include <stdint.h>
 
-#define PIX_BUF_SIZE (180 * 180 * 4)
+#define PIX_BUF_SIZE (180 * 200 * 4)
 static uint8_t pix_buf[PIX_BUF_SIZE];
 EMSCRIPTEN_KEEPALIVE uint8_t *get_pix_buf() { return pix_buf; }
 
-#define PT_BUF_SIZE (128 * 2)
+#define PT_BUF_SIZE (256 * 2)
 static float pt_buf[PT_BUF_SIZE];
 EMSCRIPTEN_KEEPALIVE float *get_pt_buf() { return pt_buf; }
 
-EMSCRIPTEN_KEEPALIVE void rasterize_outline(int w, int h, int n)
+EMSCRIPTEN_KEEPALIVE void rasterize_fill(int w, int h, int n,
+  float r, float g, float b, float opacity, float t)
 {
-  pix_buf[0] = pix_buf[1] = pix_buf[2] = pix_buf[3] = 255;
+  // Clear texture
+  for (int i = 0; i < w * h * 4; i++) pix_buf[i] = 0;
+}
+
+EMSCRIPTEN_KEEPALIVE void rasterize_outline(int w, int h, int n,
+  float r, float g, float b)
+{
+  for (int i = 0; i < n; i++) {
+    int x = (int)pt_buf[i * 2 + 0];
+    int y = (int)pt_buf[i * 2 + 1];
+    if (x >= 0 && x < w && y >= 0 && y < h) {
+      pix_buf[(y * w + x) * 4 + 0] = (int)(r * 255);
+      pix_buf[(y * w + x) * 4 + 1] = (int)(g * 255);
+      pix_buf[(y * w + x) * 4 + 2] = (int)(b * 255);
+      pix_buf[(y * w + x) * 4 + 3] = 255;
+    }
+  }
 }
