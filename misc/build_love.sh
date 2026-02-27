190939120633b7b3827e76e2beb15ca7289cec78
Ayu <claire@ayu.land>
2026-02-27 16:07:20 +0800

    Reproducible builds

--- misc/build_love.sh
+++ misc/build_love.sh
@@ -1,6 +1,17 @@
-BOON=~/Downloads/boon-macos-amd64/boon
-TARGET=${1:-all}
-${BOON} build . --target ${TARGET}
-
-# Alternative:
-# zip release/Blow\ Something.love -r aud fnt img main.lua src -9
+if [ "$1" = "boon" ]; then
+  BOON=boon
+  TARGET=${1:-all}
+  ${BOON} build . --target ${TARGET}
+else
+  rm -f release/Blow\ Something.love
+  t=$(mktemp -d)
+  wd=$PWD
+  cp -pr aud fnt img main.lua src "$t/"
+  (
+    cd "$t/" || exit
+    pwd
+    find . -exec touch -t 198001010000 {} +
+    zip "$wd/release/Blow Something.love" -Xr * -9
+  )
+  rm -rf "$t"
+fi
