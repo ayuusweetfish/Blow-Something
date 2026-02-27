fc132de18c48541b7e97d0dbdb504ec23d8c6fc7
Ayu <claire@ayu.land>
2026-02-27 16:43:33 +0800

    Add Lua minifier

--- misc/build_love.sh
+++ misc/build_love.sh
@@ -10,8 +10,15 @@ else
   (
     cd "$t/" || exit
     pwd
+    if [ "$1" = "min" ]; then
+      for i in $(find . -type f -name '*.lua'); do
+        node "$wd/misc/luamin-env/node_modules/luamin/bin/luamin" -f "$i" > "$t/_tmp"
+        mv "$t/_tmp" "$i"
+      done
+    fi
     find . -exec touch -t 198001010000 {} +
     find . -type f -print | sort | zip "$wd/release/Blow Something.love" -X -@ -9
+    echo "$wd/release/Blow Something.love"
   )
   rm -rf "$t"
 fi
